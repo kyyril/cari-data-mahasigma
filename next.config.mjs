5fbcb77b96377f910b847346bdd0c2e6574b6226
kyyril <khairilrahman55@gmail.com>
2024-09-08 13:23:08 +0700

    change root

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/mhs", // Replace with your subfolder
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import MhsList from "@/components/MhsList";
-import { InputWithButton } from "@/components/SearchInput";
 export default function Home() {
   return <h1>halo page</h1>;
 }
